5873857e373438218f37b91fece4900aabb6a3e0
6Morpheus6 <wanton-decay@gmx.net>
2025-04-22 12:18:48 +0200

    install with uv, remove fs.link

--- install.js
+++ install.js
@@ -28,15 +28,9 @@ module.exports = {
         venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install devicetorch",
-          "pip install -r requirements.txt"
+          "uv pip install devicetorch",
+          "uv pip install -r requirements.txt"
         ]
-      }
-    },
-    {
-      method: "fs.link",
-      params: {
-        venv: "app/env"
       }
     }
   ]
